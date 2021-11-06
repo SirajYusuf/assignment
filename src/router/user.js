59156139b8d3a402ee63cf9466b1d3fd03246ae7
SirajYusuf <gssirajyusuf99@gmail.com>
2021-11-06 15:51:48 +0530

    commited on saturday noon

--- src/router/user.js
+++ src/router/user.js
@@ -6,12 +6,29 @@ const jwt=require('jsonwebtoken')
 const axios = require('axios').default
 const createUser = require('../controllers/createUser')
 const loginUser = require('../controllers/loginUser')
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 
 router.post('/createUser',createUser)
 
 router.post('/login',auth,loginUser)
 
-router.get('/getData',async(req,res)=>{
+router.post('/pdf',async(req,res) => {
+    const email =  req.body.email
+    const password = req.body.password
+const doc = new PDFDocument();
+// Create a document
+const createPdf = () => {
+    doc.fontSize(25).text(`email :${email},password: ${password}`)
+    doc.pipe(fs.createWriteStream('output.pdf'));
+    doc.addPage().fontSize(25).text('Testing new page')
+    doc.end();
+}
+    createPdf()
+    res.send('success')
+})
+
+router.get('/getData',auth,async(req,res)=>{
     await User.find({}).sort('-createdAt').then((data)=>{
         res.status(200).send({
             status:200,
@@ -43,4 +60,9 @@ router.post('/logout',auth,async(req,res)=>{
     })
 })
 
+router.patch('/update',auth,async(req,res)=> {
+    req.user.email = undefined
+    await req.user.save()
+    res.send()
+})
 module.exports = router
